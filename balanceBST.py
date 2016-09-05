034866b03fa77173e6a66c5781f3018d13bb87cb
epicZeezy <aamansaray2@gmail.com>
2016-09-05 09:13:22 -0400

    Create balanceBST.py

---
+++ balanceBST.py
@@ -0,0 +1,24 @@
+Program takes array as input and inserts elements in to a BST in a way that the tree is balaned and height is minimal
+
+def balance(arr, middle, left, right):
+  if(len(arr) == 1):
+    toReturnNode = BSTNode(arr[0])
+    return toReturnNode
+  else:
+    middle = len(arr)/2
+    left = middle - 1
+    right = middle + 1
+    newRoot = BSTNode(arr[middle])
+    leftN = balance(arr[0:left], middle, left, right)
+    newRoot.left = leftN
+    rightN = balance(arr[right:len(arr)], middle, left, right)
+    newRoot.right = rightN
+    
+def main():
+  arr = input.split(' ')
+  middle = len(arr)/2
+  left = middle - 1
+  right = middle + 1
+  balance(arr, middle, left, right)
+  
+  main()
